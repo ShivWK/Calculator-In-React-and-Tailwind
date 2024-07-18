c16e754a6483d3626c234a34bfbe54721b2a2821
Admin <shivendrawk@gmail.com>
2024-07-19 03:43:59 +0530

    UI done

--- src/Components/KBDButtons/KBDButtons.jsx
+++ src/Components/KBDButtons/KBDButtons.jsx
@@ -6,7 +6,7 @@ export default function KBDButtons(props){
     let context = useContext(ColorContext);
 
     return (
-        <button className={`md:h-12 h-11 ${props.customClass} text-2xl
+        <button className={`md:h-12 h-11 ${props.customClass} text-2xl mb-1
          rounded-lg flex items-center justify-center  active:translate-y-1 transition-all duration-75 ${context.kbdbgColor} ${context.KbdkeytextColor} `} style={{boxShadow : `0 6px 0 0 ${context.shadowColor}`}}>{props.children}</button>
     )
 }
